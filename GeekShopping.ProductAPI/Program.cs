9e8019118753ddc6e4bb3190c2cd9c5c8586b7fd
thiago_osim <thiago_osim@hotmail.com>
2025-05-25 22:45:41 -0300

    Ajustado implementação do swagger no program.cs

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -11,7 +11,10 @@ builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connceti
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "GeekShopping" });
+});
 
 var app = builder.Build();
 
