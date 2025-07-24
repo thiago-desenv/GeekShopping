7e4c64767e6fa8648cfeca57c98c265a90c9ff66
thiago_osim <thiago_osim@hotmail.com>
2025-07-24 19:57:24 -0300

    Adiciona interface no roductRepository e injetado sql context e mapper

---
+++ GeekShopping.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model.Context;
+
+namespace GeekShopping.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly MySQLContext _context;
+        private readonly IMapper _mapper;
+
+        public ProductRepository(MySQLContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public Task<IEnumerable<ProductVO>> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductVO> FindById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductVO> Create(ProductVO vo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductVO> Update(ProductVO vo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
