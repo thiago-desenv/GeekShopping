d94ddeff1c4a07bab3e4e152f574e04736929f74
thiago_osim <thiago_osim@hotmail.com>
2025-08-25 23:46:01 -0300

    Criado dados para input no banco de dados

---
+++ GeekShopping.ProductAPI/Migrations/20250826023921_SeedProductDataTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProductDataTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[,]
+                {
+                    { 2L, "Camiseta", "Uma linda camiseta da cor vermelha", "http://localhost:5000/camiseta", "Camiseta", 49.9m },
+                    { 3L, "Bermuda", "Uma linda bermuda da cor preta", "http://localhost:5000/camiseta", "Bermuda", 89.9m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+        }
+    }
+}
--- GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -4,9 +4,31 @@ namespace GeekShopping.ProductAPI.Model.Context
 {
     public class MySQLContext : DbContext
     {
-        public MySQLContext() {}
-        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {}
+        public MySQLContext() { }
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product()
+            {
+                Id = 2,
+                Name = "Camiseta",
+                Price = new decimal(49.90),
+                Description = "Uma linda camiseta da cor vermelha",
+                ImageURL = "http://localhost:5000/camiseta",
+                CategoryName = "Camiseta"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product()
+            {
+                Id = 3,
+                Name = "Bermuda",
+                Price = new decimal(89.90),
+                Description = "Uma linda bermuda da cor preta",
+                ImageURL = "http://localhost:5000/camiseta",
+                CategoryName = "Bermuda"
+            });
+        }
     }
 }
